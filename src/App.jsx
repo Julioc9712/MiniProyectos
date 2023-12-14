a3ec622eda3ab6c590e1bf8f60d83f50f44b923a
Julio <juliocmr97@gmail.com>
2023-12-14 15:47:40 -0500

    proyecto arreglo dia14

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Home from './router/Home';
-import Roadmap from './router/Roadmap';
-import WeatherApi from './router/WeatherApi';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./router/Home";
+import Roadmap from "./router/Roadmap";
+import WeatherApi from "./router/WeatherApi";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <Home/>
+    path: "/",
+    element: <Home />,
   },
-  
+
   {
-    path:"/Roadmap",
-    element: <Roadmap/>
+    path: "/Roadmap",
+    element: <Roadmap />,
   },
   {
-    path:"/Weather",
-    element: <WeatherApi/>
+    path: "/Weather",
+    element: <WeatherApi />,
   },
-  
 ]);
 
 function App() {
-
-
   return (
     <>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
