5a9506859e4a2be3a7954e71cc0cedcb1f0b0c59
Julio <juliocmr97@gmail.com>
2023-12-14 15:37:28 -0500

    cambios14

--- src/components/CardApp/CardApp.jsx
+++ src/components/CardApp/CardApp.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
-import './CardApp.css'
+import React from "react";
+import "./CardApp.css";
 
 function CardApp() {
-    return (
-        <>
-        <h1 className='titulo'>"Weatheria-Roadmap Manage"</h1>
-        <div className='container-card'>
-            <div className="card" >
-                <img src="/image/weather.avif" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">React Weather</h5>
-                    <p className="card-text">Miniproyecto React de clima: consumo de API con fetch y uso de useState y useEffect para mostrar datos meteorológicos según la ubicación.</p>
-                    <a href="/Weather" className="btn btn-primary">Ir a React Weather</a>
-                </div>
-            </div>
-            <div className="card" >
-                <img src="/image/roadmap.jfif" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">React CRUD</h5>
-                    <p className="card-text">Mini proyecto de React CRUD, creando una lista de objetivos de desarrollo frontend con funciones de agregar, editar y eliminar</p>
-                    <a href="/Roadmap" className="btn btn-primary">Ir a React CRUD</a>
-                </div>
-            </div>
+  return (
+    <>
+      <h1 className="titulo">"Weatheria-Roadmap Manage"</h1>
+      <div className="container-card">
+        <div className="card">
+          <img src="/image/weather.avif" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">React Weather</h5>
+            <p className="card-text">
+              Miniproyecto React de clima: consumo de API con fetch y uso de
+              useState y useEffect para mostrar datos meteorológicos según la
+              ubicación.
+            </p>
+            <a href="/Weather" className="btn btn-primary">
+              Ir a React Weather
+            </a>
+          </div>
         </div>
-        </>
-    )
+        <div className="card">
+          <img src="/image/roadmap.jfif" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">React CRUD</h5>
+            <p className="card-text">
+              Mini proyecto de React CRUD, creando una lista de objetivos de
+              desarrollo frontend con funciones de agregar, editar y eliminar
+            </p>
+            <br />
+            <a href="/Roadmap" className="btn btn-primary">
+              Ir a React CRUD
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default CardApp
+export default CardApp;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import './Nav.css'
+import React from "react";
+import "./Nav.css";
 
 function Nav() {
   return (
     <>
-    <div className='nav'>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark " >
-        <div className="container-fluid" >
-        <a href="/" title="Free Weather API" target="_blanck"><img src='/image/logo.png' alt="Weather data by WeatherAPI.com" border="0" className='img-weahter'/></a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link " aria-current="page" href="/">Home</a>
-              <a className="nav-link" href="/Weather">Weather</a>
-              <a className="nav-link" href="/Roadmap">Roadmap</a>
-              
-
+      <div className="nav">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+          <div className="container-fluid">
+            <a
+              href="https://portafolioreactjulioc97.netlify.app"
+              title="Portafolio web"
+              target="_blanck"
+            >
+              <img src="/image/logo.png" className="img-weahter" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link " aria-current="page" href="/">
+                  Home
+                </a>
+                <a className="nav-link" href="/Weather">
+                  Weather
+                </a>
+                <a className="nav-link" href="/Roadmap">
+                  Roadmap
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
     </>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
